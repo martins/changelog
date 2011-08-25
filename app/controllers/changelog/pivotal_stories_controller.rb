e1f8df2a03ea986647bd1a99860085aca567b813
Martins Zakis <Martins.Zakis@tieto.com>
2011-08-25 17:02:17 +0300

    possibility to delete stories from view added;

--- app/controllers/changelog/pivotal_stories_controller.rb
+++ app/controllers/changelog/pivotal_stories_controller.rb
@@ -15,9 +15,15 @@ class Changelog::PivotalStoriesController < ApplicationController
   def update
     @story = Changelog::PivotalStory.find(params[:id])
     if @story.update_attributes(:title => params[:title])
-      redirect_to :action => :index, :notice => 'Information updated', :version => params[:version]
+      redirect_to changelog_pivotal_stories_url(:version => params[:version]), :flash => {:notice => 'Story updated'}
     else
       render :action => :index
     end
+  end
+
+  def destroy
+    @story = Changelog::PivotalStory.find(params[:id])
+    @story.destroy
+    redirect_to(changelog_pivotal_stories_url, :flash => {:notice => 'Story deleted'})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Changelog::Application.routes.draw do
   namespace :changelog do
     get :release_notes
     get :current_release
-    resources :pivotal_stories, :only => [:index, :update]
+    resources :pivotal_stories, :only => [:index, :update, :destroy]
     resources :versions
   end
   root :to => 'changelog#release_notes'
