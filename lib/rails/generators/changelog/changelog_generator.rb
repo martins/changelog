d60751354d4f590bd85355f8df188af0e44d6f32
Martins Zakis <Martins.Zakis@tieto.com>
2011-08-31 10:49:19 +0300

    added test files copying to generator;

--- lib/rails/generators/changelog/changelog_generator.rb
+++ lib/rails/generators/changelog/changelog_generator.rb
@@ -30,4 +30,31 @@ class ChangelogGenerator < Rails::Generators::Base
     end
   end
 
+  def copy_test_files
+     print "   \e[1m\e[34mquestion\e[0m   Do you want to copy test files for this gem to spec directory? They are written using rspec, capybara, factory_girl and database_cleaner. Copy files? [y/n] "
+     begin
+       answer = gets.chomp
+     end while not answer =~ /[yn]/i
+     if answer =~ /y/i
+       copy_file File.join(Changelog::Engine.root,'spec/models/version_spec.rb'), 'spec/models/version_spec.rb'
+       if File.exists?('spec/factories.rb')
+         f = File.open('spec/factories.rb', 'r+')
+         content = f.read
+         if content.include?('Changelog::PivotalStory')
+           puts "   \e[1m\e[34mexisting\e[0m   factories.rb already include Changelog::PivotalStory"
+         else
+           f2 = File.open(File.join(Changelog::Engine.root, 'spec/factories.rb'))
+           addon = f2.read
+           f2.close
+           content << "\n" << addon
+           f.write(content)
+         end
+         f.close
+       else
+         copy_file File.join(Changelog::Engine.root, 'spec/factories.rb'), 'spec/factories.rb'
+       end
+       copy_file File.join(Changelog::Engine.root, 'spec/spec_helper.rb'), 'spec/spec_helper.rb'
+     end
+   end
+
 end
