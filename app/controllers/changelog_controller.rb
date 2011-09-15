bb42eef04d2f523965d582d336b532fc1b53e0ef
Martins Zakis <martins.zakis@tieto.com>
2011-09-15 12:51:07 +0300

    changelog controller gets data from yam;

--- app/controllers/changelog_controller.rb
+++ app/controllers/changelog_controller.rb
@@ -1,18 +1,16 @@
 class ChangelogController < ApplicationController
   respond_to :html, :json
   def release_notes
-    versions = Changelog::Version.with_stories
-    @relese_notes = []
-    if versions.present?
-      versions.each do |version|
-        @relese_notes << Changelog::Version.parse_version(version)
-      end
-    end
+    @relese_notes = Changelog::Release.get_release_notes
     respond_with @relese_notes
   end
+
   def current_release
-    version = Changelog::Version.latest
-    @current_release = version && Changelog::Version.parse_version(version)
+    @current_release = Changelog::Release.get_current_release
     respond_with @current_release
+  end
+
+  def index
+    @data = Changelog::Release.get_yaml_data
   end
 end
