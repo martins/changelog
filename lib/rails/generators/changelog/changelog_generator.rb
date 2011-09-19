38b096835160452d90010a7331358b89fd80fa87
Martins Zakis <martins.zakis@tieto.com>
2011-09-19 15:48:05 +0300

    updated test files;

--- lib/rails/generators/changelog/changelog_generator.rb
+++ lib/rails/generators/changelog/changelog_generator.rb
@@ -1,5 +1,4 @@
 require 'rails/generators'
-require 'rails/generators/migration'
 
 class ChangelogGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
@@ -15,30 +14,13 @@ class ChangelogGenerator < Rails::Generators::Base
     copy_file 'pivotaltracker_initializer.rb', 'config/initializers/pivotaltracker.rb'
   end
 
-  def copy_test_files
-     print "   \e[1m\e[34mquestion\e[0m   Do you want to copy test files for this gem to spec directory? They are written using rspec, capybara, factory_girl and database_cleaner. Copy files? [y/n] "
+  def copy_test_file
+     print "   \e[1m\e[34mquestion\e[0m   Do you want to copy test files for this gem to spec directory? They are written using rspec. Copy files? [y/n] "
      begin
        answer = gets.chomp
      end while not answer =~ /[yn]/i
      if answer =~ /y/i
        copy_file File.join(Changelog::Engine.root,'spec/models/version_spec.rb'), 'spec/models/version_spec.rb'
-       if File.exists?('spec/factories.rb')
-         f = File.open('spec/factories.rb', 'r+')
-         content = f.read
-         if content.include?('Changelog::PivotalStory')
-           puts "   \e[1m\e[34mexisting\e[0m   factories.rb already include Changelog::PivotalStory"
-         else
-           f2 = File.open(File.join(Changelog::Engine.root, 'spec/factories.rb'))
-           addon = f2.read
-           f2.close
-           content << "\n" << addon
-           f.write(content)
-         end
-         f.close
-       else
-         copy_file File.join(Changelog::Engine.root, 'spec/factories.rb'), 'spec/factories.rb'
-       end
-       copy_file File.join(Changelog::Engine.root, 'spec/spec_helper.rb'), 'spec/spec_helper.rb'
      end
    end
 
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -1,38 +1,35 @@
 require 'spec_helper'
 
 describe Changelog::Version do
-  context "validation" do
-    it "should generate correct name from version numbers" do
-      version = Factory(:version)
-      version.name.should == "#{version.major}.#{version.minor}.#{version.build}"
-    end
-    it "should NOT allow to save wrong version numbers" do
-      version = Factory.build(:version, :minor => nil)
-      version.valid?.should be_false
-      version = Factory.build(:version, :major => 'asd')
-      version.valid?.should be_false
-    end
-    it "should NOT allow to save versions with same name" do
-      version1 = Factory(:version, :build => 9)
-      version2 = Factory.build(:version, :build => 9)
-      version2.valid?.should be_false
-    end
+  before do
+    @raw_data = [
+      {:changelog_version =>
+        {:user_stories => [
+          {:user_story => {:accepted_at => "2011-08-18", :story_type => "bug", :title => "User can get infinite ammount of money and stuff."}},
+          {:user_story => {:accepted_at => "2011-08-19", :story_type => "feature", :title => "user should see current2."}}],
+        :release_date => "2011-09-10", :name => "0.5.9"}},
+      {:changelog_version =>
+        {:user_stories => [{:user_story => {:accepted_at => "2011-08-25", :story_type => "feature", :title => "user should be able to see tasks"}}],
+        :release_date => "2011-09-21", :name => "1.1.0"}},
+      {:changelog_version => {:user_stories => [], :release_date => "2011-11-19", :name => "1.1.1"}}]
+
   end
-  context "stories" do
-    before do
-      @version=Factory(:version)
-      3.times do
-        Factory(:story, :version_id => @version.id)
-      end
-      Factory(:version, :release_date => Date.tomorrow)
-    end
-    it "should correctly return latest version" do
-      Changelog::Version.latest.should == @version
-    end
-    it "should correctly return only versions with stories" do
-      version_array=Changelog::Version.with_stories
-      version_array.count.should == 1
-      version_array[0].should == @version
-    end
+  it "should correctly format raw input data" do
+    formated_data=Changelog::Release.parse_content(@raw_data)
+    formated_data.count.should == @raw_data.count
+    formated_version = formated_data.first[:changelog_version]
+    formated_version[:id].should == 1
+    formated_version[:major].should == '0'
+    formated_version[:minor].should == '5'
+    formated_version[:build].should == '9'
+    formated_version[:user_stories].first[:user_story][:story_id].should == 1
+  end
+  it "should remove temporary data before saving to data to yaml" do
+    parsed_data = Changelog::Release.remove_temporary_data(@raw_data)
+    parsed_data.count.should == @raw_data.count
+    parsed_version = parsed_data.first[:changelog_version]
+    parsed_version[:id].should be_nil
+    parsed_version[:major].should be_nil
+    parsed_version[:user_stories].first[:user_story][:story_id].should be_nil
   end
 end
