31c3426bb416f2dbabdf4f5cf32984c028325042
Martins Zakis <martins.zakis@tieto.com>
2011-10-14 14:03:30 +0300

    spec files updated;

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -1,33 +1,59 @@
 require 'spec_helper'
 
 describe "Release" do
-  it "Should be able to return raw yaml data" do
-    raw_data = Changelog::Release.get_raw_yaml_data
-    raw_data.first.present?.should be_true
-    raw_data.each{|version| version[:changelog_version].present?.should be_true }
-    raw_data.count.should == 3
-  end
-  it "Should be able to return fromated data" do
-    formated_data = Changelog::Release.get_formated_yaml_data
-    formated_data.first.present?.should be_true
-    formated_data.each do |version|
-      version[:release_date].present?.should be_true
-      version[:name].present?.should be_true
+  context "with existing yaml file" do
+    it "Should be able to return yaml data" do
+      data = Changelog::Release.get_yaml_data
+      data.present?.should be_true
+      data.each{|version| version['version'].present?.should be_true }
+      data.count.should == 3
     end
-    formated_data.count.should == 3
+    it "Should be able to return release notes" do
+      release_notes = Changelog::Release.get_release_notes
+      release_notes.count.should == 2
+      version = release_notes.last
+      version['release_date'].should == "2011-10-21"
+      version['features'].count.should == 3
+      version['features'].first['body'].should == 'Imlemented first feature.'
+    end
+    it "Should be able to return current release" do
+      current_release = Changelog::Release.get_current_release
+      current_release['release_date'].should == "2011-11-11"
+      current_release['features'].first['body'].should == 'Implemented one feature for second release.'
+      current_release['bugs'].first['body'].should == 'Fixed one bug for second release.'
+    end
   end
-  it "Should be able to return release notes" do
-    release_notes = Changelog::Release.get_release_notes
-    release_notes.count.should == 2
-    version = release_notes.last
-    version[:version].should == "2011-10-21"
-    version[:user_stories][:features].count.should == 3
-    version[:user_stories][:features].first[:body].should == 'Imlemented first feature.'
+  context "with empty yaml file" do
+    before(:all) do
+      FileUtils.move(File.join(Rails.root,'changelog.yml'), File.join(Rails.root, 'changelog_hide.yml'))
+      FileUtils.touch(File.join(Rails.root,'changelog.yml'))
+    end
+    it "Should not raise error when trying to get release notes" do
+      release_notes = Changelog::Release.get_release_notes
+      release_notes.should == []
+    end
+    it "Should not raise error when trying to get current release" do
+      current_release = Changelog::Release.get_current_release
+      current_release.should be_nil
+    end
+    after(:all) do
+      FileUtils.move(File.join(Rails.root,'changelog_hide.yml'), File.join(Rails.root, 'changelog.yml'))
+    end
   end
-  it "Should be able to return current release" do
-    current_release = Changelog::Release.get_current_release
-    current_release[:version].should == "2011-11-11"
-    current_release[:user_stories][:features].first[:body].should == 'Implemented one feature for second release.'
-    current_release[:user_stories][:bugs].first[:body].should == 'Fixed one bug for second release.'
+  context "without yaml file" do
+    before(:all) do
+      FileUtils.move(File.join(Rails.root,'changelog.yml'), File.join(Rails.root, 'changelog_hide.yml'))
+    end
+    it "Should not raise error when trying to get release notes" do
+      release_notes = Changelog::Release.get_release_notes
+      release_notes.should == []
+    end
+    it "Should not raise error when trying to get current release" do
+      current_release = Changelog::Release.get_current_release
+      current_release.should be_nil
+    end
+    after(:all) do
+      FileUtils.move(File.join(Rails.root,'changelog_hide.yml'), File.join(Rails.root, 'changelog.yml'))
+    end
   end
 end
